7bd3bd28004a552a10fa5fa143f8e2bdc4edf9f6
Geraldo Santo <geraldo@axys.com.br>
2023-05-19 18:06:20 -0300

    Versão do processo de Spring Initializr

---
+++ src/test/java/br/com/axys/spring_course/sec2/Sec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.axys.spring_course.sec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sec2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
